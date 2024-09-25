e8e625130b6ec79dce1b1860a87e76cb94913b1f
Miriam Blanco <miriam.blanco.lm@gmail.com>
2024-09-25 15:41:42 +0200

    modificacion outputs

--- modules/load_balancer/outputs.tf
+++ modules/load_balancer/outputs.tf
@@ -1,3 +1,1 @@
-output "lb_public_ip" {
-  value = azurerm_public_ip.main_lb_public_ip.ip_address
-}
+
--- modules/virtual_machines/outputs.tf
+++ modules/virtual_machines/outputs.tf
@@ -1,3 +1,1 @@
-output "vm_ids" {
-  value = [for vm in azurerm_linux_virtual_machine.vm : vm.id]
-}
+
